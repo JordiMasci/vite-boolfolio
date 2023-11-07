d032a767f7d5f684dd76f76a8a20b8b1cb753e98
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-11-07 16:48:39 +0100

    feat: importati componenti dentro router.js

--- src/pages/BlogPage.vue
+++ src/pages/BlogPage.vue
@@ -3,19 +3,17 @@ import ProjectList from "../components/projects/ProjectList.vue";
 
 export default {
   data() {
-   return {
-     title: "Hello world"
-   }
- }
+    return {
+      title: "Hello world",
+    };
+  },
 
- // components: {
- //   MyComponent,
- // },
+  components: { ProjectList },
 };
 </script>
 
 <template>
-   <h1>{{ title }}</h1>
+  <h1>{{ title }}</h1>
 </template>
 
 <style lang="scss" scoped></style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,25 @@
 import { createWebHistory, createRouter } from "vue-router";
 
+// importa componenti
+import HomePage from '../pages/HomePage.vue'
+import BlogPage from '../pages/BlogPage.vue'
+
 const router = createRouter({
     // aggiungi history
     history: createWebHistory(),
 
     // aggiungi rotte
     routes: [
-        
+        {
+            name: 'homepage',
+            path: '/',
+            component: HomePage
+        },
+        {
+            name: 'blog',
+            path: '/blog',
+            component: BlogPage
+        }
     ]
 });
 
