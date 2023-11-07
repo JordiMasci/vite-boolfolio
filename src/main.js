40569cb07201dcce8db02d955ee3bf149e0269d5
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-11-07 16:53:42 +0100

    feat: importo router in main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from "vue";
 import "./assets/scss/style.scss";
 
+// importa router
+import {router} from './router/router.js'
+
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+createApp(App).use(router).mount("#app");
